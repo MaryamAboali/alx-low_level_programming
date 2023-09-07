766f7f879672afa6467a74b4bc0b6f934fef5f56
MaryamAboali <maryamaboali33@gmail.com>
2023-09-07 14:56:25 -0700

    1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,13 @@
 /**
  * main - Entry point
  *
- * description: A C program that prints with put function
+ * Description: A C progam that prints with printf function.
  *
- * Return: Alawys 0 (success)
+ * Return: Always 0 (success)
 */
 
-int main (void)
+int main(void)
 {
-puts ("\"Programming is like building a multilingual puzzle");
-return (0);
+        printf("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
