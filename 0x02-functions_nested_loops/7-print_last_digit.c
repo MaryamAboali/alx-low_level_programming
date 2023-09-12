5ab641227d5f086d9a6cf84578e1b65ee2cd72a1
MaryamAboali <maryamaboali33@gmail.com>
2023-09-12 15:10:00 -0700

    a

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,12 +10,12 @@
 
 int print_last_digit(int n)
 {
-	int lastDigit;
+	int LastDigit;
 
 	if (n < 0)
-		lastDigit = -1 * (n % 10);
+		LastDigit = -1 * (n % 10);
 	else
-		lastDigit = n % 10;
-	_putchar(lastDigit + '0');
+		LastDigit = n % 10;
+	_putchar(LastDigit + '0');
 	return (LastDigit);
 }
