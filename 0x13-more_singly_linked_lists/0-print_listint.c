58ab6de1f26252b8155844d4b88ff3113f69e7a3
MaryamAboali <maryamaboali33@gmail.com>
2023-10-23 08:30:24 -0700

    a

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,13 @@
  */
 size_t print_listint(const listint_t *h);
 {
-	size_t i = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
+		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
-		i++;
 	}
-	return (i);
+	return (nodes);
 }
