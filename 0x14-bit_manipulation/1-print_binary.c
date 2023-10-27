373c716357c028e036656b3442585a9f0e4841ae
MaryamAboali <maryamaboali33@gmail.com>
2023-10-27 13:51:12 -0700

    a

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -14,12 +14,12 @@ void print_binary(unsigned long int n)
 	{
 		if (n & 1L << --bit)
 		{
-			-putchar('1');
+			_putchar('1');
 			printed++;
 		}
 		else if (printed)
-			-putchar('0');
+			_putchar('0');
 	}
 	if (!printed)
-		-putchar('0');
+		_putchar('0');
 }
