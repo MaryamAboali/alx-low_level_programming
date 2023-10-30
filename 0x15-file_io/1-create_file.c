a2c137b5d51ed9fcdf7f8873e887feb376802e34
root <root@DESKTOP-EFUUGP7.localdomain>
2023-10-31 00:42:24 +0200

    a

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
  * _strlen - returns the length of a string
  * @s: he string whose length to check
- * 
+ *
  * Return: integer length of string
  */
 int _strlen(char *s)
@@ -21,7 +21,7 @@ int _strlen(char *s)
  * create_file - creats a file
  * @filename: name of file to create
  * @text_content: text to write
- * 
+ *
  * Return: 1 on success 0 on failure
  */
 int create_file(const char *filename, char *text_content)
